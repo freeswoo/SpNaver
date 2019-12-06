440a1b04234fc0e3811d0d476a67055e61697a40
freeswoo <freeswoo@naver.com>
2019-12-06 13:54:36 +0900

    네이버검색시작

---
+++ src/main/java/com/biz/naver/config/NaverConfigSample.java
@@ -0,0 +1,8 @@
+package com.biz.naver.config;
+
+public class NaverConfigSample {
+
+	public final static String NaverClientId = "OOuhKqLTI0ZtLBUZf0UT";
+	public final static String NaverClientSecret = "tgUXo9W4Rs";
+	
+}
